2b3b6413c632faab9acdf8abaf20cb87d04f783a
sneakysnakesstudio <sneakysnakes@proton.me>
2026-07-25 18:28:44 +0200

    Player Movement

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum FpsLimit
     {
-        
+        FPS60 = 60,
+        FPS120 = 120
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Performance")]
+    [SerializeField] private FpsLimit fpsLimit = FpsLimit.FPS60;
+
+    private void Awake()
     {
-        
+        SetFpsLimit(fpsLimit);
+    }
+
+    public void SetFpsLimit(FpsLimit newLimit)
+    {
+        fpsLimit = newLimit;
+
+        // Wyłączenie VSync, żeby limit FPS działał prawidłowo.
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = (int)fpsLimit;
+    }
+
+    public void Set60Fps()
+    {
+        SetFpsLimit(FpsLimit.FPS60);
+    }
+
+    public void Set120Fps()
+    {
+        SetFpsLimit(FpsLimit.FPS120);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,57 +1,95 @@
-using System;
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-  [Header("Speed")] [SerializeField] private float walkSpeed = 5f;
-  [Header("Speed")] [SerializeField] private float runSpeed = 5f;
+    [Header("Speed")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 8f;
 
-  [Header("References")]
-  [SerializeField] private Transform cameraTransform;
-  [SerializeField] private InputActionReference moveAction;
-  
-  private CharacterController _charecterController;
-  private Vector2 _moveInput;
+    [Header("References")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference speedAction;
 
-  private void Awake()
-  {
-    _charecterController = GetComponent<CharacterController>();
-  }
+    [Header("Gravity")]
+    [SerializeField] private float gravity = -12f;
+    [SerializeField] private float groundedVelocity = -2f;
 
-  private void OnEnable()
-  {
-    moveAction.action.Enable();
-    
-    moveAction.action.performed += StoreMovementInput;
-    moveAction.action.canceled += StoreMovementInput;
-  }
+    private CharacterController _characterController;
+    private Vector2 _moveInput;
+    private float _verticalVelocity;
 
-  private void OnDisable()
-  {
-    moveAction.action.performed -= StoreMovementInput;
-    moveAction.action.canceled -= StoreMovementInput;
-  }
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
 
-  private void StoreMovementInput(InputAction.CallbackContext context)
-  {
-    _moveInput = context.ReadValue<Vector2>();
-  }
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+        speedAction.action.Enable();
 
-  private void Update()
-  {
-    HandleMovement();
-  }
+        moveAction.action.performed += StoreMovementInput;
+        moveAction.action.canceled += StoreMovementInput;
+    }
 
-  private void HandleMovement()
-  {
-      var move = cameraTransform.TransformDirection(new Vector3(_moveInput.x, 0f, _moveInput.y).normalized);
-      var currentSpeed = walkSpeed;
-      var finalMove = move * currentSpeed;
-      
-      _charecterController.Move(finalMove * Time.deltaTime);
-  }
-  
-  
+    private void OnDisable()
+    {
+        moveAction.action.performed -= StoreMovementInput;
+        moveAction.action.canceled -= StoreMovementInput;
+
+        moveAction.action.Disable();
+        speedAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        HandleGravity();
+        HandleMovement();
+    }
+
+    private void StoreMovementInput(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void HandleGravity()
+    {
+        if (_characterController.isGrounded && _verticalVelocity < 0f)
+        {
+            _verticalVelocity = groundedVelocity;
+        }
+        else
+        {
+            _verticalVelocity += gravity * Time.deltaTime;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 moveDirection =
+            cameraRight * _moveInput.x +
+            cameraForward * _moveInput.y;
+
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f);
+
+        bool isSprinting = speedAction.action.IsPressed();
+        float currentSpeed = isSprinting ? sprintSpeed : walkSpeed;
+
+        Vector3 finalMove = moveDirection * currentSpeed;
+        finalMove.y = _verticalVelocity;
+
+        _characterController.Move(finalMove * Time.deltaTime);
+    }
 }
